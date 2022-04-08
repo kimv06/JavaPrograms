b0ba39cf86a41b9496270a408cd5eda38a39d9ba
kimv06 <monalpodder12@gmail.com>
2022-04-08 18:00:33 +0530

    second day java class

---
+++ day2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class day2{
+    public static void main(String args[]){
+        /*int a=99;
+        switch(a){
+            case 'a':
+                System.out.println("34");
+                break;
+            case 'b':
+                System.out.println("12");
+                break;
+            case 'c':
+                System.out.println("19");
+                break;
+            double a=0.2+0.1;
+            System.out.println(a);
+            Scanner sc = new Scanner(System.in);
+            double c=sc.nextDouble();
+            System.out.println(a==c);
+        }
+        int i=1;
+        while(i++ <10){
+            System.out.println(i);
+        }*/
+        
+    }
+}
