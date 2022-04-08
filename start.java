fcf2db5533f213a0a06b0555fd5f7c7d72a47231
kimv06 <monalpodder12@gmail.com>
2022-04-08 18:01:41 +0530

    starting code

---
+++ start.java
@@ -0,0 +1,9 @@
+public class start{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int sum=0;
+        sum=a+b;
+        System.out.println(sum);
+    }
+}
