0e05ac399e69ed4b16829d96a1490dd5a4f04c78
kimv06 <monalpodder12@gmail.com>
2022-07-10 20:56:24 +0530

    sum of two numbers without using binary operator

--- sum_without_binary_operator.java
+++ sum_without_binary_operator.java
@@ -1,10 +1,10 @@
 import java.util.*;
-class sum
+class sum_without_binary_operator
 {
     static int add(int a , int b)
     {
         int c = 0;
-        while(b!=0)
+        while(b != 0)
         {
             c = a&b;
             a = a^b;
@@ -20,6 +20,7 @@ class sum
         a = sc.nextInt();
         System.out.println("Enter another number: ");
         b = sc.nextInt();
-        System.out.println("The sum of two numbers =" +add(a,b));
+        //add ob = new add();
+        System.out.println("The sum of two numbers = " +add(a,b));
     }
 }
